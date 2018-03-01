b9d824f4b1da603c780bcb551499361fb08b06a5
kobad <kobad.dogsy@gmail.com>
2018-03-01 23:32:15 +0900

    [Mod] test main script.

--- main.py
+++ main.py
@@ -1,49 +1,41 @@
-import dropbox
-from pygments.lexers import PythonLexer
-from prompt_toolkit import prompt
-from prompt_toolkit.layout.lexers import PygmentsLexer
-from prompt_toolkit.contrib.completers import WordCompleter
-from tqdm import tqdm
-import os
+# This is a test script.
 
-access_token = os.environ["DROPBOX_TOKEN"]
-export_format = dropbox.paper.ExportFormat('markdown')
+from dbx_client import (
+    DBXClient
+)
+import inquirer
 
-# Dropbox client
-dbx = dropbox.Dropbox(access_token)
-# All document list
-docs_list = dbx.paper_docs_list()
-# { folder_name: [doc_id]}
-folders = {}
-# { doc_id: doc_info }
-docs = {}
-print("Get documents")
-i = 0
-for id in tqdm(docs_list.doc_ids):
-    i += 1
-    if i > 10:
-        break
-    try:
-        folder = dbx.paper_docs_get_folder_info(id).folders[0]
-        if folder.name in folders:
-            folders[folder.name].append(id)
-        else:
-            folders[folder.name] = [id]
-        docs[id] = dbx.paper_docs_download(id, export_format)
-    except Exception as e:
-        pass
 
-completer = WordCompleter(list(folders.keys()))
-selected_folder = prompt('Select Folder: ',
-                         completer=completer,
-                         lexer=PygmentsLexer(PythonLexer))
+def main():
+    c = DBXClient()
+    c.get_folders()
 
-selected_docs_ids = folders[selected_folder]
-titles = []
-for id in selected_docs_ids:
-    titles.append(docs[id][0].title)
+    questions = [
+        inquirer.List('name',
+                      message='Select folder',
+                      choices=list(c.folders.keys()),
+                      ),
+    ]
 
-completer = WordCompleter(titles)
-selected_title = prompt('Select Doc: ',
-                         completer=completer,
-                         lexer=PygmentsLexer(PythonLexer))
+    answer = inquirer.prompt(questions)
+    folder_name = answer['name']
+    c.get_docs_with_folder_name(folder_name)
+
+    questions = [
+        inquirer.List('name',
+                      message='Select doc',
+                      choices=c.get_doc_titles_with_folder_name(folder_name),
+                      ),
+    ]
+
+    answer = inquirer.prompt(questions)
+    title = answer['name']
+
+    doc = c.get_doc_with_title(title)
+
+    print(doc.title)
+    print(doc.data)
+
+
+if __name__ == '__main__':
+    main()
